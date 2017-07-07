3fd9ecb55c340b94ef417e77214c7b1770d9783f
appdreams <pvinicius@gmail.com>
2017-07-07 17:23:11 -0300

    Segundo Commit

---
+++ app/src/main/java/br/com/consultec/corretor/activity/TesteActivity.java
@@ -0,0 +1,19 @@
+package br.com.consultec.corretor.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.consultec.corretor.R;
+
+public class TesteActivity extends BaseActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teste);
+
+        setUpToolbar("CORRETOR");
+    }
+}
